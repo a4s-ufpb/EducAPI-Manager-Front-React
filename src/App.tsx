8d0e3814ac162529b7b4faf55efc70debec58a32
André Victor <andrehsvictor@gmail.com>
2024-02-24 11:43:24 -0300

    Add routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ContextPage from "./pages/ContextPage";
+import ChallengePage from "./pages/ChallengePage";
+import IndexPage from "./pages/IndexPage";
+
 function App() {
   return (
     <>
-      <h1>EducAPI Manager</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<IndexPage />} />
+            <Route path="contexts" element={<ContextPage />} />
+            <Route path="challenges" element={<ChallengePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
