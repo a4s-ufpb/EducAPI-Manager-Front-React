622bcea10389276fb154f390e9234221b4bce002
André Victor <andrehsvictor@gmail.com>
2024-02-24 11:43:53 -0300

    Add IndexPage component

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, Container, Divider, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function IndexPage() {
+  const navigate = useNavigate();
+  const redirectToContexts = () => {
+    navigate("/contexts");
+  };
+  const redirectToChallenges = () => {
+    navigate("/challenges");
+  };
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        border: "1px solid lightgray",
+        borderRadius: "5px",
+        padding: "10px",
+      }}
+    >
+      <Typography variant="h5" align="center" gutterBottom>
+        Welcome to EducAPI Manager
+      </Typography>
+      <Divider variant="middle" sx={{ margin: "10px 0" }} />
+      <Box display="flex" justifyContent="space-around">
+        <Button variant="contained" onClick={redirectToContexts}>
+          See Contexts
+        </Button>
+        <Button variant="contained" onClick={redirectToChallenges}>
+          See Challenges
+        </Button>
+      </Box>
+    </Container>
+  );
+}
